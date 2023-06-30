f55172d1fd3c63c2714739c66b519c9fc510b056
biel.lil <78098857+biellil@users.noreply.github.com>
2023-06-30 17:16:48 -0300

    30/06
    Finalizando Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Header } from "./components/Header"
-import {Post} from "./post"
+import {Post} from "./components/post"
 
 
 import styles from './App.module.scss';
---
+++ src/components/post.jsx
@@ -0,0 +1,10 @@
+export function Post() {
+
+
+    return (
+    <div>
+   
+    </div>
+    );
+    
+}
